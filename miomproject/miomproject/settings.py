badf95d23381101f7c1736ba6fee4bcccbbf677c
Olgy Jeangilles <tlogipaisupremology@yahoo.com>
2020-07-26 21:58:02 -0400

    added custom s3

--- miomproject/miomproject/settings.py
+++ miomproject/miomproject/settings.py
@@ -133,7 +133,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
-
+AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_S3_CUSTOM_DOMAIN")
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
@@ -146,5 +146,5 @@ if APP_PHASE == "development":
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 else:
-    STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}"
+    STATIC_URL = f"{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}"
     STATIC_ROOT = 'static/'
